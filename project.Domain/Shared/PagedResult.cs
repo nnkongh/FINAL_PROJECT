37b752ba5b14874fd696ade95c49247e24c1e989
nnkongh <haukong1308@gmail.com>
2026-07-15 01:20:01 +0700

    Add pagination

---
+++ project.Domain/Shared/PagedResult.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace project.Domain.Shared
+{
+    public class PagedResult<T>
+    {
+        public IReadOnlyList<T> Items { get; set; } = Array.Empty<T>();
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    }
+}
