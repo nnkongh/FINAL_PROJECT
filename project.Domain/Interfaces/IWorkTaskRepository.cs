aed218eef849d0efdeeb12d7cebaeb34c8295bd6
nnkongh <haukong1308@gmail.com>
2026-07-15 01:26:47 +0700

    Add nullable option for groupId

--- project.Domain/Interfaces/IWorkTaskRepository.cs
+++ project.Domain/Interfaces/IWorkTaskRepository.cs
@@ -5,7 +5,7 @@ namespace project.Domain.Interfaces
     public interface IWorkTaskRepository : IGenericRepository<WorkTask>
     {
         Task<List<WorkTask>> GetTasksByGroupIdAsync(int groupId, TasksStatus? taskStatus = null, TaskPriority? taskPriority = null);
-        Task<List<WorkTask>> GetTasksByUserIdAsync(int groupId, int userId, TasksStatus? taskStatus = null, TaskPriority? taskPriority = null);
+        Task<List<WorkTask>> GetTasksByUserIdAsync(int userId, int? groupId = null, TasksStatus? taskStatus = null, TaskPriority? taskPriority = null);
         Task<WorkTask?> GetWithCreatorAndAssigneeByIdAsync(int id);
         Task<List<WorkTask>> GetOverdueTasksByGroupIdAsync(int groupId);
     }
