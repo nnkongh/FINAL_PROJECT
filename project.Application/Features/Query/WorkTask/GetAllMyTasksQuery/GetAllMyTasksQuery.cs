b7907d132d14b83f1dab0e994ed777a84d9072e4
nnkongh <haukong1308@gmail.com>
2026-07-15 01:36:31 +0700

    fix doesnt exists in the name space

---
+++ project.Application/Features/Query/WorkTask/GetAllMyTasksQuery/GetAllMyTasksQuery.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MediatR;
+using project.Application.ModelsDto;
+using project.Domain.Interfaces;
+using project.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project.Application.Features.Query.WorkTask.GetAllMyTasksQuery
+{
+    public sealed record GetAllMyTasksQuery(int UserId) : IRequest<Result<IReadOnlyList<TaskModel>>>
+    {
+    }
+    internal sealed class GetAllMyTasksHandler : IRequestHandler<GetAllMyTasksQuery, Result<IReadOnlyList<TaskModel>>>
+    {
+        private readonly IWorkTaskRepository _taskRepository;
+        private readonly IMapper _mapper;
+        public GetAllMyTasksHandler(IMapper mapper, IWorkTaskRepository taskRepository)
+        {
+            _mapper = mapper;
+            _taskRepository = taskRepository;
+        }
+
+        public async Task<Result<IReadOnlyList<TaskModel>>> Handle(GetAllMyTasksQuery request, CancellationToken cancellationToken)
+        {
+
+            var tasks = await _taskRepository.GetTasksByUserIdAsync(request.UserId, null);
+
+            var mapped = _mapper.Map<IReadOnlyList<TaskModel>>(tasks);
+
+            return Result.Success(mapped);
+        }
+    }
+}
