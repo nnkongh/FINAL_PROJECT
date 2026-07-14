0f3908e54b85be2c84c230d668127199acb9bd0d
nnkongh <haukong1308@gmail.com>
2026-07-15 01:04:41 +0700

    Add Nullable status

--- project.Domain/Models/TaskHistory.cs
+++ project.Domain/Models/TaskHistory.cs
@@ -12,18 +12,21 @@ namespace project.Domain.Models
         public int Id { get; private set; }
         public int TaskId { get; private set; }
         public int ChangedBy { get; private set; }
-        public TasksStatus OldStatus { get; private set; }
-        public TasksStatus NewStatus { get; private set; }
+        public TasksStatus? OldStatus { get; private set; }
+        public TasksStatus? NewStatus { get; private set; }
         public DateTime ChangedAt { get; private set; } = DateTime.UtcNow;
         public WorkTask Task { get; private set; }
         public UserApp User { get; private set; }
 
         private TaskHistory() { }
         public static TaskHistory Create(WorkTask task, int changedBy,
-            TasksStatus oldStatus, TasksStatus newStatus)
+            TasksStatus? oldStatus = null, TasksStatus? newStatus = null)
         {
-            if (oldStatus == newStatus)
+            if (oldStatus != null && newStatus != null)
+            {
+                if (oldStatus == newStatus)
                 throw new DomainException("Trạng thái mới phải khác trạng thái cũ");
+            }
 
             return new TaskHistory
             {
