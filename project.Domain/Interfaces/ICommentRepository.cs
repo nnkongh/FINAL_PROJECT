f54b600a549c51f0d9e37730c1ebb2b3c0f702d1
nnkongh <haukong1308@gmail.com>
2026-07-15 01:12:58 +0700

    remove duplicate code

--- project.Domain/Interfaces/ICommentRepository.cs
+++ project.Domain/Interfaces/ICommentRepository.cs
@@ -10,6 +10,5 @@ namespace project.Domain.Interfaces
     public interface ICommentRepository : IGenericRepository<Comment>
     {
         Task<IReadOnlyList<Comment>> GetCommentsByTaskIdAsync(int taskId);
-        Task<Comment?> GetCommentByTaskIdAsync(int taskId);
     }
 }
